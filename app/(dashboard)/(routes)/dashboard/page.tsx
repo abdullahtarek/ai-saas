3563749a9262af872ffb0f17e87b15d26f1d6d16
Abdullah Tarek <abdullahtarek@rocketmail.com>
2023-09-02 10:21:55 +0200

    edit subtitle

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -54,7 +54,7 @@ export default function DashboardPage() {
           Explore the power of AI
         </h2>
         <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
-          Chat with the smartest AI - Experience the power of AI
+          Experience the power of AI
         </p>
       </div>
       <div className="px-4 md:px-20 lg:px-32 space-y-4">
